44f1ac59bc79815454ea198402be934ef1677059
Prithvi159 <prcsrock@gmail.com>
2025-10-05 09:36:17 +0530

    bug fix

--- frontend/FinancialPortfolioTracker-Frontend/src/api/api.js
+++ frontend/FinancialPortfolioTracker-Frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://financialportfoliotracker-v5d8.onrender.com";
 
-export const createApiInstance = (token) => {
+export default createApiInstance = (token) => {
   const instance = axios.create({
     baseURL: API_BASE_URL,
   });
